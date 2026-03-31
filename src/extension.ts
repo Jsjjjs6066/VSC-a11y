41dc969dd2c9222408595f9a0c3ff3c026a07d8b
Leon Žlender <jsjjjs6066@outlook.com>
2026-03-31 16:04:11 +0200

    Add ishihara test webview and container view

--- src/extension.ts
+++ src/extension.ts
@@ -2,11 +2,23 @@ import * as vscode from 'vscode';
 import { activate as activateNoLines } from './no_lines';
 import { activate as activateColors } from './colors';
 import { activate as activateSize } from './size';
+import { activate as activateIshihara } from './ishihara';
+import { IshiharaWebviewProvider } from './webview/ishiharaWebviewProvider';
 
 export function activate(context: vscode.ExtensionContext) {
 	activateNoLines(context);
 	activateColors(context);
 	activateSize(context);
+
+	const ishiharaProvider = new IshiharaWebviewProvider(context.extensionUri);
+	context.subscriptions.push(
+		vscode.window.registerWebviewViewProvider(
+			IshiharaWebviewProvider.viewType,
+			ishiharaProvider
+		)
+	);
+
+	activateIshihara(context);
 }
 
 export function deactivate() {}
