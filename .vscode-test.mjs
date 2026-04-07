7f03b3a3905a3c44d5d49dc6528c16349c6562d8
CHAOS2CONSOLE <chaos.console.2@gmail.com>
2026-04-07 21:02:40 +0200

    Testiranje

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -2,4 +2,8 @@ import { defineConfig } from '@vscode/test-cli';
 
 export default defineConfig({
 	files: 'out/test/**/*.test.js',
+	workspaceFolder: '.',
+	useInstallation: {
+		fromPath: 'C:\\Users\\filip\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe',
+	},
 });
